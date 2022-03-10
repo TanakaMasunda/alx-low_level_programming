3dcff1ea5df6c35159e76da030bc2b92998adce8
TanakaMasunda <masundatanaka@gmail.com>
2022-03-10 15:40:36 -0800

    c intro first

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,8 @@
-#!/bin/bash
-gcc puts("Programming is like building a multilingual puzzle");   
+#include <stdio.h>
+/**
+ * main-prints the phrase "programming is like building a multilingual puzzle
+ * Return : 0 if exited properly,non-zero otherwise*/
+int main (void)
+{
+       	puts("\"Programming is like building a multilingual puzzle"); 
+	return (0); }   
