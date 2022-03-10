5cb79a956283a39250a2571a8ff832ee9b93858a
TanakaMasunda <masundatanaka@gmail.com>
2022-03-10 15:28:38 -0800

    c intro first

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,2 @@
-# !/ bin / bash
-init main(void) { puts("Programming is like building a multilingual puzzle");
-       	getchar() ;
-       	return 0; }  
+#!/bin/bash
+gcc puts("Programming is like building a multilingual puzzle");   
