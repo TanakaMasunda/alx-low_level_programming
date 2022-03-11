15e64c1d9ba4afaf6a4272755936775deb3be631
TanakaMasunda <masundatanaka@gmail.com>
2022-03-10 16:17:08 -0800

    c intro first

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/* main - print phrase
+ * Return: 0 if exited properly
+ */
+int main(void)
+{
+	printf("\"with proper grammar,but the outcome is a piece of art");
+	return (0);
+}
