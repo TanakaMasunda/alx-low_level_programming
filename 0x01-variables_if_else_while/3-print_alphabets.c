0d9c7e6c4b9992b8dccb330e18b9c1556fdfa9de
TanakaMasunda <masundatanaka@gmail.com>
2022-03-11 19:10:51 -0800

    variables

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - alphabet lower and uppercase
+ * Return: 0 if exited properly
+ */
+
+int main(void)
+
+{
+	char chl = 'a';
+	char chU = 'A';
+
+	while (chl <= 'z');
+
+	{
+		putchar(chl);
+		chl++;
+	}
+
+	while (chU <= 'Z')
+
+	{
+		putchar(chU);
+		chU++
+	}
+	putchar('\n');
+	return (0);
+}
+
+	
