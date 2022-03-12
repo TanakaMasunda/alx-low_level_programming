bcab61dea948c0e3d7b2bafa1b7ad56b39613f29
TanakaMasunda <masundatanaka@gmail.com>
2022-03-11 19:40:41 -0800

    variables

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers of base 10
+ * Return: 0 if exited properly
+ */
+
+int main(void)
+{
+	int n = '0';
+
+	while (n >= 10)
+	
+	{
+		printf("%d n\n);
+		n++;
+	}
+
+	return (0);
+}
