8e46b9c840161cddd02d14a5e916763653b51801
TanakaMasunda <masundatanaka@gmail.com>
2022-03-11 17:57:54 -0800

    variables if else while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - lowercase alphabet
+ * Return: 0 if exited properly
+ */
+
+int main(void)
+{
+	char = a;
+	for (char = a, char <= z, char ++);
+	putchar(char);
+	return (0);
+}
