31e8ed84de77e5a79739d4db98c249d4cd50d213
TanakaMasunda <masundatanaka@gmail.com>
2022-03-11 19:31:08 -0800

    variables

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - alphabet skip q and e
+ * Return: 0 if exited properly
+ */
+
+int main(void)
+
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	
+	{
+		if (ch == 'q' || ch == 'e')
+		{
+			ch++;
+		}
+		else
+		{
+			putchar(ch);
+			ch++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
