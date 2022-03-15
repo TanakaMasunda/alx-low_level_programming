9f3002d0f1d5eedf992cbefc35e9d1cefd5173d4
TanakaMasunda <masundatanaka@gmail.com>
2022-03-15 13:37:55 -0700

    nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /* Declaration of print_alphabet function */
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
 /* Prototype of _putchar function */
-int putchar(char ch);
+int int(char ch);
 
 /* prototype of print_putchar function */
 void print_alphabet(void);
