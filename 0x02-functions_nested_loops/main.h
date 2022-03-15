19876f1c0ae7080088bac10152e3f9841e1c9a7d
TanakaMasunda <masundatanaka@gmail.com>
2022-03-15 07:34:02 -0700

    nested loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,7 +4,7 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @: The character to print
+ * @c: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately
