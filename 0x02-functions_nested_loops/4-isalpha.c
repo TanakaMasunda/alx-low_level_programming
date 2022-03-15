4aad919f8f5e53e12ec091aede22709edb582d9d
TanakaMasunda <masundatanaka@gmail.com>
2022-03-15 16:55:42 -0700

    nested loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,11 +6,13 @@ int _isalpha(int c);
 /**
  * _isalpha - is an alphabetic character
  *
+ * @c: the character to check
+ *
  * Return: 1 if its uppercase or lowercase
  */
 
 int _isalpha(int c)
 
 {
-       	return((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
