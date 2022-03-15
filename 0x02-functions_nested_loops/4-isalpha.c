028fc6d4616b684ef0517535e4671f69a71907f7
TanakaMasunda <masundatanaka@gmail.com>
2022-03-15 16:49:02 -0700

    nested loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -12,5 +12,5 @@ int _isaalpha(int c);
 int _isaalpha(int c)
 
 {
-       	return (c >= 'a' && c <= 'z' && c >= 'A' && c <= 'Z');
+       	return((c >= 'a' && c <= 'z' || (c >= 'A' && c <= 'Z'));
 }
