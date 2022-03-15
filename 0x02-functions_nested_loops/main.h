7b01917f8170aca995dc7c509d8cc981f34f3aa4
TanakaMasunda <masundatanaka@gmail.com>
2022-03-15 14:19:07 -0700

    nested loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,9 @@
 /* Prototype of _putchar function */
-int putchar(ch);
+int _putchar(char c);
 
 /* prototype of print_putchar function */
+void print_putchar(void);
+
+/* pritotype of the alphabet fucntion */
 void print_alphabet(void);
+
