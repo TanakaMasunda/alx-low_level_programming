3f6e6e05ce437b64945eaea5f75f0eeddc7638c3
TanakaMasunda <masundatanaka@gmail.com>
2022-03-15 04:44:30 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print putchar
+ * Return: 0 if exited properly
+ */
+
+int main(void)
+{
+
+	char ch =  _putchar;
+	putchar(ch);
+	putchar("\n");
+	return (0);
+}
+
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+nsted loops
