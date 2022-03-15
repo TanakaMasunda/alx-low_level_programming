745c59a3ba8ceef27a0b5cc9f543aafa9b13abcb
TanakaMasunda <masundatanaka@gmail.com>
2022-03-15 13:32:44 -0700

    nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,21 @@
-#include <stdio.h>
+#include "main.h"
+
+/* Declaration of print_alphabet function */
+void print_alphabet(void);
 
 /**
  * main - Entry point
  *
  * Return: always 0 (success)
  */
-
-int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+/**
+ * print_alphabet - writes alphabet string to stdout
+ */
+void print_alphabet(void)
 {
 	
 	char ch = 'a';
@@ -16,5 +25,4 @@ int main(void)
 		ch++;
 	}
 	putchar('\n');
-	return (0);
 }
