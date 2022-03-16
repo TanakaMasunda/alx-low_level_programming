1fd1ccbad8eeaf93776e7f87f1f3c59ab2b3dfc9
TanakaMasunda <masundatanaka@gmail.com>
2022-03-15 17:27:09 -0700

    nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,14 +14,16 @@ int print_sign(int n)
 
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar(+);
 	}
-	else if (n == 0)
-	{
-		_putchar(0);
-	}
-	else
+	else if (n < 0)
 	{
 		_putchar(-1);
 	}
+	else
+	{
+		_putchar(0);
+	}
+	return (0);
+
 }
