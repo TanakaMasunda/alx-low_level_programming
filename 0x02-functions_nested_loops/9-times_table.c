7225d2f83ef06e2df71c97cbba55fdd9837adfa8
TanakaMasunda <masundatanaka@gmail.com>
2022-03-15 20:05:26 -0700

    nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,10 +11,11 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 
-	{ for ( j = 0; j <= 9; j++0)
+	{ for ( j = 0; j <= 9; j++)
 
 		{
 			n = i * j;
+
 			if ((n / 10 ) == 0)
 			{
 				if (j != 0)
@@ -22,7 +23,7 @@ void times_table(void)
 					_putchar(' ');
 				_putchar(n + '0');
 
-				if (j == 9);
+				if (j == 9)
 				 continue;
 				 _putchar(' ');
 				 _putchar(' ');
@@ -31,7 +32,9 @@ void times_table(void)
 			{
 				_putchar((n / 10) + '0');
 				_putchar((n % 10) + '0');
+				
 				if (j ==9)
+
 					continue;
 				_putchar(',');
 				_putchar(' ');
