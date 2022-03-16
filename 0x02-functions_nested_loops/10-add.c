cbbfa7b5b0ff3e6e12f71c75ee743ed100f3a7ed
TanakaMasunda <masundatanaka@gmail.com>
2022-03-15 20:27:58 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - check add 
+ *
+ * @a: integer a
+ *
+ * @b: integer b
+ *
+ * Return: result to add a and b
+ */
+
+int add(int a, int b)
+
+{
+	return (a + b);
+}
