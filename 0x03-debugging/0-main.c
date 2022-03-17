30e9beb3c101bc9fa3bc8f1cd612b2dc7663ad2d
TanakaMasunda <masundatanaka@gmail.com>
2022-03-17 03:26:26 -0700

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,7 +4,6 @@ void positive_or_negative(int i);
 
 /**
  * main - tests function that prints if i is positive or negative
- *
  * Return: 0 if success
  */
 
@@ -14,7 +13,9 @@ int main(void)
 	int i;
 
 	i = 0;
+
 	positive_or_negative(i);
+
 	return (0);
 
 }
