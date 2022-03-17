9c4fd42757748569a5e39e654c7198021d67cc8f
TanakaMasunda <masundatanaka@gmail.com>
2022-03-17 03:10:44 -0700

    debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,2 +1,12 @@
-/* prototype of the positive_or_negative function */
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+
 void positive_or_negative(int i);
+int largest_number(int a , int b , int c);
+void print_remaining_days(int month , int day ,int year);
+int convert_day(int month , int day);
+
+#endif /* MAIN_H */
