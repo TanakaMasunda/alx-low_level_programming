ebcc5e1f51dea408ab856bdf23ac2770e7bcb7b1
TanakaMasunda <masundatanaka@gmail.com>
2022-03-17 15:03:56 -0700

    more functions

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - print more numbers
+ * Return: 0 on success
+ */
+
+void more_numbers(void)
+{
+	int i = '0';
+
+	for (; i <= '9'; i++)
+	{
+		int n = '0';
+
+		if (; n <= '14'; n++)
+			_putchar(n);
+	}
+	_putchar('\n');
+}
+
