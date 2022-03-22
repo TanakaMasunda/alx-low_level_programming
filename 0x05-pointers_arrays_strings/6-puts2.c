a6484c2ee8df5d5fdbadc7b980e340df74e6c14b
TanakaMasunda <masundatanaka@gmail.com>
2022-03-21 17:48:19 -0700

    pointers arrays strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - prints 1 char of a string of 2
+ * @str: input string
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+
+		_putchar('\0');
+		break;
+	}
+	if (count % 2 == 0)
+		_putchar(str[count]);
+	count++;
+}
+}
