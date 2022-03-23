2d060d4adb51bb5f1fe5bf5d64b614eda76d2eaa
TanakaMasunda <masundatanaka@gmail.com>
2022-03-23 15:58:16 -0700

    more pointers arrays

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,7 +5,7 @@
 #include <string.h>
 
 char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, unsigned long n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a,int n);
