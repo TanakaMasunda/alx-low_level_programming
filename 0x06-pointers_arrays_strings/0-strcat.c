9686c63893923a98763a447a079d5e8695e15849
TanakaMasunda <masundatanaka@gmail.com>
2022-03-23 14:49:49 -0700

    more pointers

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenate two strings
+ * Return: void
+ */
+
+char *_strcat(char *dest, char *src);
+{
+	unsigned long i;
+	unsigned long n;
+
+	n = strlen(dest);
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[n + 1] = src[i];
+	}
+	i++;
+
+	dest[n + i] = '\0';
+
+	return (dest);
