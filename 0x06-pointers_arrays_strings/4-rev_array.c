deca6037ec22c827223d144749b3a03b088ec096
TanakaMasunda <masundatanaka@gmail.com>
2022-03-23 16:38:00 -0700

    more pointers arrays

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reverse_array - print array in reverse
+ * @a: first integer
+ * @n: second integer
+ * Return: 0 if success
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+
+	int array[n];
+
+	for (i = 0; i < n && i != 0; i--;)
+		putchar (i);
+	return (0);
+}
