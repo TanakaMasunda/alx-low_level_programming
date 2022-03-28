376a714bbdfbdcd87d474ba6f2f9cd3f8c6fe322
TanakaMasunda <masundatanaka@gmail.com>
2022-03-28 16:17:25 -0700

    more pointers more

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,7 +9,15 @@
 
 char *_strchr(char *s, char c)
 {
-	s = strchr(c);
+	unsigned int i = 0;
 
-	return (0);
+	for (; *(s + i) != '\0''; i++)
+		if (*(s + i) == c)
+
+			return (s + i);
+
+	if (*(s + i) == c)
+		return (s + i);
+
+	return ('\0');
 }
