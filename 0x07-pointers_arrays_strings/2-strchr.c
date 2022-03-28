e1bcb305b39b2a6d371ac421067bfc29469e91a3
TanakaMasunda <masundatanaka@gmail.com>
2022-03-28 16:12:23 -0700

    more pointers more

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strchr - returns pointer to first occurance in a string
+ * @c: the character
+ * @s: pointer
+ * Return: null if not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	s = strchr(c);
+
+	return (0);
+}
