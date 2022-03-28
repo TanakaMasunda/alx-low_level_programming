2c6f5918219623bd1a146b25f7843122b342176d
TanakaMasunda <masundatanaka@gmail.com>
2022-03-28 15:18:27 -0700

    more pointers more

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+#include <string.h>
+
+/**
+ * _memcpy - copy memory area
+ * @dest: destination of memory area
+ * @src: source of memory area
+ * @n: n bytes
+ * Return: pointer to destination
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(dest + i) = *(src + i);
+
+		return (dest);
+}
