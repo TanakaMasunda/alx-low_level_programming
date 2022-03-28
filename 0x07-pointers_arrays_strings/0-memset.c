dada1b003231f2518126a7497a538b5259590a3b
TanakaMasunda <masundatanaka@gmail.com>
2022-03-28 14:32:08 -0700

    more more pointers

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+#include <string.h>
+
+/**
+ * _memset - fill memory with constant byte
+ * @b: character to be store constant byte
+ * @n: integer
+ * @s: pointer
+ * Return: pointer to the memory s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	return (s);
+}
