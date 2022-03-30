98b5809c0b3d53cb544e121fce0e6ce0bce5a87a
TanakaMasunda <masundatanaka@gmail.com>
2022-03-30 16:09:54 -0700

    RECURSIONS

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,19 +9,12 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s =='\0')
+	if (*s == '\0')
 	{
-		_putchar(*s);
-	_putchar(s + 1);
+		_putchar('\n');
+		return;
 	}
-}
 
-int main(void)
-{
-
-	char s[50];
-	
-	_putchar(s);
-
-	return (0);
+		_putchar(*s);
+	_puts_recursion(s + 1);
 }
