2e37053662da274891d76aa128dd9235cee57317
TanakaMasunda <masundatanaka@gmail.com>
2022-03-30 16:59:28 -0700

    RECURSIONS

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - string length
+ * @s: string pointer
+ * Return: length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		return (1 + _strlen_recursion(s +1));
+	}
+	{
+
+	return (0);
+	}
+}
