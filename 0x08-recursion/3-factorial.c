a75d075e898ef35f317bf41b021fcbcfba8311eb
TanakaMasunda <masundatanaka@gmail.com>
2022-03-30 17:13:35 -0700

    RECURSIONS

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -12,8 +12,12 @@ int factorial(int n)
 	{
 		return (-1);
 	}
-	else if (n >= 0)
+	else if (n == 0) || ( n == 1)
 	{
-		n * (n - 1)!;
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
 	}
 }
