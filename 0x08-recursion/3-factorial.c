3379c76842d1f143e39b6a8dce99d70d93ffea4d
TanakaMasunda <masundatanaka@gmail.com>
2022-03-30 17:08:47 -0700

    RECURSIONS

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * factorial - factorial on numbers
+ * @n: integer
+ * Return: -1 if theres error
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n >= 0)
+	{
+		n * (n - 1)!;
+	}
+}
