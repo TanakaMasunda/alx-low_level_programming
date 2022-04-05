96a792a29df49378ce85e41042481e562295868c
TanakaMasunda <masundatanaka@gmail.com>
2022-04-05 13:01:27 -0700

    malloc free

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -5,6 +5,7 @@
 /**
  * _strdup - returns a function to a string which is a duplic of str
  * @str: the original string
+ * @len: length of strings
  * Return: null if str is null, if insufficient memory was available
  * On success, the _strdup function returns a pointer to the duplicated string
  */
@@ -13,16 +14,23 @@ char *_strdup(char *str)
 {
 	unsigned int i;
 	char c;
-	int len;
+	unsigned int len;
+	
+	while (str[len] !='\0')
+		len++;
 
 	str = malloc((len + 1) * sizeof(char));
-	if (str == NULL || len == 0)
+	if (str == NULL || str[len] == '0')
 	{
 		return (NULL);
 	}
+
+	char c;
+
 	for (i = 0; i < len; i++)
 	{
 		str[i] = c;
 	}
+
 	return (str);
 }
