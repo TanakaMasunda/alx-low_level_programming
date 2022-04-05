2e24855736c8b51acfdb76893d1a5c430c68ef8c
TanakaMasunda <masundatanaka@gmail.com>
2022-04-05 16:42:27 -0700

    malloc free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,17 +21,17 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	ptr = (int**)malloc(width * sizeof(int *));
+	ptr = (int **)malloc(width * sizeof(int *));
 	if (ptr == NULL)
 		return (NULL);
 
 	for (i = 0; i < width; i++)
 	{
-		ptr[i] = (int*)malloc(height * sizeof(int));
+		ptr[i] = (int *)malloc(height * sizeof(int));
 		if (ptr[i] == NULL)
 			return (NULL);
 	}
-	
+
 	while (width < height)
 	{
 		while (height < width)
