68eca295f696151ec9bd8beeb61fc9c3c26f6ad2
TanakaMasunda <masundatanaka@gmail.com>
2022-04-05 13:58:20 -0700

    malloc free

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,13 +15,13 @@ char *_strdup(char *str)
 	unsigned int len;
 	char *ptr;
 
-	len == 0;
+	len = 0;
 
 	while (str[len]	!= '\0')
 		len++;
 
 	ptr = malloc((len + 1) * sizeof(char));
-	if (str == NULL || ptr == NULL)
+	if (ptr == NULL)
 	{
 		return (NULL);
 	}
@@ -29,6 +29,8 @@ char *_strdup(char *str)
 	for (i = 0; i < len; i++)
 	{
 		str[i] = ptr[i];
+		if (str == NULL)
+			return (NULL);
 	}
 
 	return (ptr);
