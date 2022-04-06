9d890f420e62c22821e0ddd38d50666a88fd1d58
TanakaMasunda <masundatanaka@gmail.com>
2022-04-06 16:15:43 -0700

    argc argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,7 +8,7 @@
  * Return: 0 om success
  */
 
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc _attribute_((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
