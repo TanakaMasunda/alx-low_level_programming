68ba62312145ee4e5fc717dfa941671e59d9f62e
TanakaMasunda <masundatanaka@gmail.com>
2022-04-07 19:16:55 -0700

    more malloc free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -24,6 +24,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr);
 
 	free(ptr);
-	ptr = malloc(new_size)
-		return (ptr);
+	ptr = malloc(new_size);
+	
+	return (ptr);
 }
