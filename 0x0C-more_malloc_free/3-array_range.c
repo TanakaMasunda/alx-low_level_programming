2277f0342b38a0aa874072d7517b2b127377cf44
TanakaMasunda <masundatanaka@gmail.com>
2022-04-07 18:49:46 -0700

    more malloc free

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,10 +14,13 @@ int *array_range(int min, int max)
 	int *ar;
 	int i;
 
+	if (min > max)
+		return (NULL);
+
 	ar = malloc(sizeof(*ar) * ((max - min) + 1));
 
 
-	if (min > max || ar == NULL)
+	if (ar == NULL)
 		return (NULL);
 
 	for (i = 0; min <= max; i++, min++)
