69a56f762cea51d61abc4f1913d24d7217cdd1c9
TanakaMasunda <masundatanaka@gmail.com>
2022-04-12 11:46:01 -0700

    preprocessor

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,5 +1,20 @@
-#ifndef ADD
+#ifndef SUM
 
-#define ADD ((x) + (y))
+#define SUM ((x) + (y))
 
+/**
+ * main - function for adding 2 numbers
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int num1, num2;
+
+	printf("Enter any two numbers: ");
+	printf("SUM(%d, %d) = %d\n", num1, num2, SUM(num1, num2));
+
+	return (0);
+
+}
 #endif
