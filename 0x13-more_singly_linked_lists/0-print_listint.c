000fc8f0ca17d4e1673d6361805ba8a1cabe157f
TanakaMasunda <masundatanaka@gmail.com>
2022-04-26 18:32:18 -0700

    more singly lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t elements = 0;
+	size_t nodes = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		elements++;
+		nodes++;
 	}
 
-	return (elements);
+	return (nodes);
 }
