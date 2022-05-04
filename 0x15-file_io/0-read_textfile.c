25e0b7f2eb632343959a955f60d4e67c2f12b0be
TanakaMasunda <masundatanaka@gmail.com>
2022-05-03 17:31:38 -0700

    input output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * read_textfile - read textfile and print it to posix standard
+ * @filename: name of file created
+ * @letters: number of letters to be printed
+ * Return: the actual number of letters it could read and print
+ * if the file can not be opened or read,or  filename is NULL return 0
+ * if write fails or does not write the expected amount of bytes, return 0
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+
+	if (!buf)
+		return (0);
+
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
+	close(fd);
+
+	free(buf);
+
+	return (nwr);
+}
